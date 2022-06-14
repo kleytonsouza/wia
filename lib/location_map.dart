59268858f0174e53990d5cc97f658aeea850ccf8
kleytonsouza <kleytonl.souza@gmail.com>
2022-06-14 16:16:42 -0300

    add pc prograd

--- lib/location_map.dart
+++ lib/location_map.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key, required this.title}) : super(key: key);
@@ -14,100 +18,128 @@ class HomePage extends StatefulWidget {
 //-25.39954147933631, -49.250469280756825
 class _HomePageState extends State<HomePage> {
   final List<String> _locations = [
-   '1 Gabinete - Financeiro',
-   '2 Gabinete - Secretaria',
-   '3 COPEG', 
-   '4 COPAC',
-   '5 COPAP - Seção de Acompanhamento Acadêmico (SAAC)',
-   '6 COPAP - Seção de Gerenciamento Acadêmico (SGA)',
-   '7 COPAP - Unidade de Diplomas (UD)',
-   '8 COPAP - Seção de Ocupação de Vaga (SOCV)',
-   '9 COAFE - Unidade de Estágios (UE)',
-   '10 COAFE - Unidades de Atividades Formativas (UAF)',
-   '11 COSIS - Coord de Sistemas de Informação para a Gestão Acadêmica',
-   '12 CIPEAD - Unidade Administrativa',
-   '13 CIPEAD - Unidade Pedagógica',
-   '14 CIPEAD - LabCIPEAD',
-   '15 CIPEAD - Equipe Multidisciplinar'];
+    '1 Gabinete - Financeiro',
+    '2 Gabinete - Secretaria',
+    '3 COPEG',
+    '4 COPAC',
+    '5 COPAP - Seção de Acompanhamento Acadêmico (SAAC)',
+    '6 COPAP - Seção de Gerenciamento Acadêmico (SGA)',
+    '7 COPAP - Unidade de Diplomas (UD)',
+    '8 COPAP - Seção de Ocupação de Vaga (SOCV)',
+    '9 COAFE - Unidade de Estágios (UE)',
+    '10 COAFE - Unidades de Atividades Formativas (UAF)',
+    '11 COSIS - Coord de Sistemas de Informação para a Gestão Acadêmica',
+    '12 CIPEAD - Unidade Administrativa',
+    '13 CIPEAD - Unidade Pedagógica',
+    '14 CIPEAD - LabCIPEAD',
+    '15 CIPEAD - Equipe Multidisciplinar'
+  ];
   var currentSelectedValue;
-  
+  String _scanBarcode = 'Unknown';
+  String coord = "";
+
+  @override
+  void initState() {
+    super.initState();
+    scanQR();
+  }
+
+  Future<void> scanQR() async {
+    String barcodeScanRes;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          '#ff6666', 'Cancel', true, ScanMode.QR);
+      print(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _scanBarcode = barcodeScanRes;
+      coord = barcodeScanRes.split(',') as String;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Você esta aqui."),
-      ),
-      body: Column(
-        children: [
+        appBar: AppBar(
+          title: Text("Você esta aqui."),
+        ),
+        body: Column(children: [
           Container(
-            color: Colors.cyan,
-            child:DropdownButton(
-              style: const TextStyle(
-                          fontSize: 12.5,
-                          color: Colors.black,
-                          fontFamily: "verdana_regular",
-                        ),
-              hint: const Text(' Please choose a location', style: TextStyle(
-                            color: Colors.black,
-                            //fontSize: 16,
-                            fontFamily: "verdana_regular",
-                          )
-                          ), 
-              value: currentSelectedValue,
-              isDense: true,
-              onChanged: (newValue) {
-                setState(() {
-                  currentSelectedValue = newValue;
-                });
-              },
-              items: _locations.map((location) {
-                return DropdownMenuItem(
-                  child: Text(location),
-                  value: location,
-                );
-              }).toList(),
-            )
-          ),
-           Container(
-             height: 580,
-              child: FlutterMap(
-                options: MapOptions(
-                  center: LatLng(-25.42901, -49.26744),
-                  zoom: 19,
+              color: Colors.cyan,
+              child: DropdownButton(
+                style: const TextStyle(
+                  fontSize: 12.5,
+                  color: Colors.black,
+                  fontFamily: "verdana_regular",
                 ),
-                layers: [
-                  TileLayerOptions(
-                      maxZoom: 22,
-                      urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                      subdomains: ['a', 'b', 'c'],
-                      attributionBuilder: (_) {
-                      return Text("");
-                      },
-                      ),
-                  MarkerLayerOptions(
-                    markers: [
-                      Marker(
-                        point: LatLng(-25.42901, -49.26744),
-                        builder: (ctx) => Icon(Icons.pin_drop, color: Colors.red),
-                      ),
-                    ],
-                  ),
-                ],
+                hint: const Text(' Please choose a location',
+                    style: TextStyle(
+                      color: Colors.black,
+                      //fontSize: 16,
+                      fontFamily: "verdana_regular",
+                    )),
+                value: currentSelectedValue,
+                isDense: true,
+                onChanged: (newValue) {
+                  setState(() {
+                    currentSelectedValue = newValue;
+                  });
+                },
+                items: _locations.map((location) {
+                  return DropdownMenuItem(
+                    child: Text(location),
+                    value: location,
+                  );
+                }).toList(),
+              )),
+          Container(
+            height: 580,
+            child: FlutterMap(
+              options: MapOptions(
+                center: LatLng(-25.42901, -49.26744),
+                zoom: 19,
               ),
-            
+              layers: [
+                TileLayerOptions(
+                  maxZoom: 22,
+                  urlTemplate:
+                      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                  subdomains: ['a', 'b', 'c'],
+                  attributionBuilder: (_) {
+                    return Text("");
+                  },
+                ),
+                MarkerLayerOptions(
+                  markers: [
+                    Marker(
+                      point: LatLng(coord[0] as double, coord[1] as double),
+                      builder: (ctx) => Icon(Icons.pin_drop, color: Colors.red),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
           Expanded(
-            child: Container(
-              //height: 5,
-              color: Color.fromRGBO(1, 63, 122, 1),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [Text( "©2021 - Universidade Federal do Paraná", textAlign: TextAlign.center, style: TextStyle(color: Colors.white ))]
-              )
-            )
-          )
-        ]
-      )
-    );
+              child: Container(
+                  //height: 5,
+                  color: Color.fromRGBO(1, 63, 122, 1),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("©2021 - Universidade Federal do Paraná",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: Colors.white))
+                      ])))
+        ]));
   }
 }
